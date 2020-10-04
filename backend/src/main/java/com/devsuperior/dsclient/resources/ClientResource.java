77f9c327c40b1407297d7b9eb21e2b74b0bc59d1
Ana Luisa Dias <analudias@live.com>
2020-10-04 11:48:06 -0300

    Client timezone, Resource annotation, sql import

--- backend/src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ public class ClientResource {
 	@Autowired
 	private ClientService service;
 	
+	@GetMapping
 	public ResponseEntity<List<ClientDTO>> findAll(){
 		List<ClientDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
